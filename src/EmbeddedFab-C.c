f37b13b9cbd706b21d39357163cd1185f43216e4
mohammed.nabil <tiger_nabil2010@yahoo.com>
2018-07-13 21:18:27 +0200

    change

--- src/EmbeddedFab-C.c
+++ src/EmbeddedFab-C.c
@@ -6,22 +6,57 @@
 //double  sensor3 = 48;
 
 
-int  input = 0;
+int  input = 1;
 
 
 
 int main(void) {
 
+	puts("please enter input :");
 	scanf("%d" , &input);
-	printf("input :  %d \r\n" , input );
+	printf("input : %d \r\n" , input);
 
 
-	//	printf("Decimal of Sensor :  %d \r\n" , sensor );
-	//	printf("Char of Sensor :  %c \r\n" , sensor );
+	//	input |= (1 << 0);
+	//	printf("input : %d\r\n" , input);
 	//
-	//	printf("Size of Sensor :  %d \r\n" , sizeof(sensor) );
+	//	input &= ~(1 << 0);
+	//	printf("input : %d\r\n" , input);
 
-	//	printf("index :  %d \r\n" ,index );
-	//	printf("index :  %c \r\n" ,index );
+
+	//	if(input ==  1)
+	//	{
+	//
+	//		printf("OK");
+	//	}
+	//	else if (input == 2)
+	//	{
+	//		printf("Failed");
+	//
+	//	}else
+	//	{
+	//		printf("Try again ");
+	//
+	//	}
+
+	switch(input)
+	{
+	case 1 :
+		printf("OK");
+
+		break;
+	case 2 :
+		printf("Failed");
+
+		break;
+	default :
+		printf("Try again ");
+
+		break;
+
+	}
+
+
+
 	return 0;
 }
