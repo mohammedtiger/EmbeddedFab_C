7fbebd0ee8c682f8e0c49c7bf99495076695f937
mohammed.nabil <tiger_nabil2010@yahoo.com>
2018-08-10 19:24:41 +0200

    new

--- src/EmbeddedFab-C.c
+++ src/EmbeddedFab-C.c
@@ -1,53 +1,33 @@
 #include "stdio.h"
+#include "stdlib.h"
 
+void printBinary(int number);
+int scanBinary();
+void printHex();
 
-char Array[11] = {0};
+//int mul (int x , int y);
+//int sub (int x , int y);
+//int sum (int return1 , int return2);
 
-char value_char[2][3] = {{1 , 2 , 3} \
-					   , {5 , 6 , 7} \
-									};
+int mul (int x , int y)
+{
+	return x*y;
+}
 
+int sub (int x , int y)
+{
+	return x-y;
 
-char *value_char_ptr ;
-
-int value_int = 0 ;
-int *value_int_ptr ;
-
-
-void sWap ( char *x , char *y);
-void SwapArrays(char *arr1 , char *arr2 , int size);
-
+}
+int sum (int x , int y)
+{
+	return x+y;
+}
 
 
 int main(void) {
 
-	for(int index = 0 ; index < 3 ; index++)
-	{
-		printf("%d" ,value_char[0][index]);
-	}
 
-	printf("\r\n");
-
-	for(int index = 0 ; index < 3 ; index++)
-	{
-		printf("%d" ,value_char[1][index]);
-	}
-
-	SwapArrays(value_char[0] , value_char[1] , 3);
-
-	printf("\r\n");
-
-	for(int index = 0 ; index < 3 ; index++)
-	{
-		printf("%d" ,value_char[0][index]);
-	}
-
-	printf("\r\n");
-
-	for(int index = 0 ; index < 3 ; index++)
-	{
-		printf("%d" ,value_char[1][index]);
-	}
 
 
 
@@ -61,24 +41,93 @@ int main(void) {
 
 
 
-void sWap ( char *x , char *y)
-{
-	char temp = *x;
-	*x = *y;
-	*y=temp;
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+void printBinary(int number)
+{
+	char bits[32] = {0};
+	int counter = 0;
+	while(number)
+	{
+		counter ++;
+
+		if(number & 1)
+		{
+			bits[counter-1] = 1;
+		}
+		else
+		{
+			bits[counter-1] = 0;
+		}
+		number = (number >> 1);
+	}
+
+	for(int index = counter-1 ; index > -1 ; index --)
+	{
+		printf("%d" , bits[index]);
+	}
 }
 
-
-
-void SwapArrays(char *arr1 , char *arr2 , int size)
+int scanBinary()
 {
+	char value [32] = {0};
+	int counter = 0;
+	int number = 0;
 
-	for(int index = 0 ; index < size ; index ++)
+	for(int index = 0 ; index < 32 ; index++)
 	{
-		sWap( &arr1[index] , &arr2[index]);
+		scanf("%c" , &value[index]);
+		if (value[index] == '\n')
+		{
+			value[index] = 0 ;
+			break;
+		}
+		counter++;
+	}
+
+	for(int index = counter-1 ; index > -1 ; index --)
+	{
+		number |= (value[counter-1-index] & 1) << index;
+	}
+	return number;
+}
+
+void printHex(int number)
+{
+	char bits[8] = {0};
+	int counter = 0;
+
+	for(int index = 0 ; index < 32 ; index+=4)
+	{
+
+		bits[index/4] = (number >> index ) & 0x0f;
+
+	}
+
+	printf("0x");
+	for(int index = 8-1 ; index > -1 ; index --)
+	{
+		if(bits[index]==10) printf("A");
+		else if(bits[index]==11) printf("B");
+		else if(bits[index]==12) printf("C");
+		else if(bits[index]==13) printf("D");
+		else if(bits[index]==14) printf("E");
+		else if(bits[index]==15) printf("F");
+		else printf("%d" , bits[index]);
+
 	}
 
 }
-
 
